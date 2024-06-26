7116397bd0dd3bc1e7123a6df600147b9ffd75b8
Busola Tom <busolatom@gmail.com>
2024-06-26 16:12:33 +0000

    Express middleware

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import express from "express";
-import recipeRouter from "./routes/recipes.js";
+import recipeRouter from "./routes/recipe.js";
+import mongoose from "mongoose";
+
+// Connect to database
+await mongoose.connect(process.env.Mongo_url);
 
 // Create Express App
-const app = express();
+const recipeapp = express();
+
+// Apply middlewares-application middleware unwraps data posted from the frontend
+recipeapp.use(express.json());
 
 // Use routes- enable us make use of other routes defined in other files
-app.use(recipeRouter);
+recipeapp.use(recipeRouter);
 
 // Listen for incoming requests
-app.listen(3000, () => {
+recipeapp.listen(3000, () => {
     console.log('App listening on port 3000');
 });
+
+
+// user password
+//  0oyaUgeseJzdWcXg
+
--- routes/recipe.js
+++ routes/recipe.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { RecipeModel } from "../models/recipe.js";
 
 // Create router
 const recipeRouter = Router();
@@ -8,8 +9,11 @@ recipeRouter.get('/recipes', (req, res) => {
     res.json('All recipes');
 });
 
-recipeRouter.post('/recipes', (req, res) => {
-    res.json('Recipe added');
+recipeRouter.post('/recipes', async (req, res) => {
+    // Add recipe to database
+    await RecipeModel.create(req.body);
+    // Return response
+    res.json('Fufu and Light Soup Recipe added');
 });
 
 recipeRouter.patch('/recipes/:id', (req, res) => {
@@ -20,6 +24,8 @@ recipeRouter.delete('/recipes/:id', (req, res) => {
     res.json(`Recipe with id ${req.params.id} deleted`);
 });
 
-
+recipeRouter.get('/recipes/:id', (req, res) => {
+    res.json(`Recipe with id ${req.params.id} provided`);
+});
 // Export router
 export default recipeRouter;
