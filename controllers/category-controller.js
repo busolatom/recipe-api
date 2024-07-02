c5b2d347983a3c71f478da3b418904b2d86ed51b
Busola Tom <busolatom@gmail.com>
2024-07-02 14:09:44 +0000

    Filter and fields{query & options in mongoDB

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,8 +2,14 @@ import { CategoryModel } from "../models/category.js";
 
 export const getCategories = async (req, res, next) => {
     try {
+        // Get query params- the structuring
+        const { limit, skip, filter, fields } = req.query;
         // Get all categories from database
-        const allCategories = await CategoryModel.find();
+        const allCategories = await CategoryModel
+            .find(JSON.parse(filter))
+            .select(JSON.parse(fields))
+            .limit(limit)
+            .skip(skip);
         // Return response
         res.status(200).json(allCategories);
     } catch (error) {
@@ -12,15 +18,15 @@ export const getCategories = async (req, res, next) => {
 }
 
 export const postCategory = async (req, res, next) => {
-    try {       
+    try {
         // Add category to database
         const newCategory = await CategoryModel.create({
             ...req.body,
-            image: req.file.filename 
+            image: req.file.filename
         });
         // Return response
         res.status(201).json(newCategory);
-        } catch (error) {
+    } catch (error) {
         next(error);
     }
 }
--- controllers/recipe-controller.js
+++ controllers/recipe-controller.js
@@ -2,71 +2,73 @@ import { RecipeModel } from "../models/recipe-model.js";
 
 // Get all recipes
 export const getRecipes = async (req, res, next) => {
-    try {
-      // Get query params- the structuring
-      const {limit, skip, search} = req.query;
-      // Get all recipes from database
-      const allRecipes = await RecipeModel.find();
-      // Return all recipes as response
-      // res.json(`Here is your requested recipe, ${allRecipes}`);
-      res.json(allRecipes);
-    } catch (error) {
-         next(error);
-    }
- }
+  try {
+    // Get query params- the structuring
+    const { limit, skip, filter } = req.query;
+    // Get all recipes from database
+    const allRecipes = await RecipeModel
+      .find(filter)
+      .limit(limit)
+      .skip(skip);
+    // Return all recipes as response
+    // res.json(`Here is your requested recipe, ${allRecipes}`);
+    res.json(allRecipes);
+  } catch (error) {
+    next(error);
+  }
+}
 
 //  Get recipe by Id
 export const getRecipe = async (req, res, next) => {
   try {
-   const oneRecipe = await RecipeModel.findById(req.params.id);
-  //  return response
-   res.status(200).json(oneRecipe);
+    const oneRecipe = await RecipeModel.findById(req.params.id);
+    //  return response
+    res.status(200).json(oneRecipe);
   } catch (error) {
-       next(error);
+    next(error);
   }
 }
 
 //  Post recipe
- export const postRecipe = async (req, res, next) => {
-    try {
-        // Add recipe to database
-        await RecipeModel.create({
-          ...req.body,
-          image: req.file.filename 
-      });
-        // Return response
-        // res.json('Fufu and Light Soup Recipe added');
-        res.json(`${req.body.name} recipe added`);
-    } catch (error) {
-          next(error);
-    }
+export const postRecipe = async (req, res, next) => {
+  try {
+    // Add recipe to database
+    await RecipeModel.create({
+      ...req.body,
+      image: req.file.filename
+    });
+    // Return response
+    // res.json('Fufu and Light Soup Recipe added');
+    res.json(`${req.body.name} recipe added`);
+  } catch (error) {
+    next(error);
   }
+}
 
 //   Update recipe
-export const updateRecipe = async (req, res, next) => 
-  {    
+export const updateRecipe = async (req, res, next) => {
   try {
     // do an update
     const newUpdate = req.body;
-    const updatedRecipe = await RecipeModel.findByIdAndUpdate(req.params.id, newUpdate, {new: true});
+    const updatedRecipe = await RecipeModel.findByIdAndUpdate(req.params.id, newUpdate, { new: true });
     // Return response
     res.status(200).json(updatedRecipe);
   } catch (error) {
     next(error);
   }
-}  
+}
 
 
 // Delete recipe
 export const deleteRecipe = async (req, res, next) => {
-   try {
+  try {
     // delete recipe by id
     const deletedRecipe = await RecipeModel.findByIdAndDelete(req.params.id);
     // return response
-     res.json(`${req.body.name} recipe with id ${req.params.id} deleted`);
-   } catch (error) {
+    res.json(`${req.body.name} recipe with id ${req.params.id} deleted`);
+  } catch (error) {
     next(error)
-   }
+  }
 }
 
 
